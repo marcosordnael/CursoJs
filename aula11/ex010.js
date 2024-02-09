300d5cf29c45fc7b9350d7d4352f8b12fb1740d3
MARCOS LEANDRO ANTUNES <158173682+marcosordnael@users.noreply.github.com>
2024-02-09 00:33:49 -0300

    site

--- aula11/ex010.js
+++ aula11/ex010.js
@@ -1,10 +1,9 @@
-var idade = 45
+var idade = 66
+console.log(`Você tem ${idade} anos.`)
 if (idade < 16) {
     console.log('Não vota!')
+} else if (idade < 18 || idade > 65) {
+    console.log('O voto é opcional')
 } else {
-    if (idade >= 16 && idade < 18){
-        console.log('O voto é opcional!')
-    } else {
-        console.log('Voto obrigatório!')
-    }
+    console.log('O voto é obrigatório')
 }
