2eb83bbb5609d1685bfa3a25e85f513714d06176
MARCOS LEANDRO ANTUNES <158173682+marcosordnael@users.noreply.github.com>
2024-02-12 22:23:48 -0300

    a

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,17 @@
-var c = 1
-while (c <= 10) {
+// var c = 1
+// do {
+//     console.log(`Passo ${c}`)
+//     c++
+// }while (c <= 12) 
+
+// var c = 1
+// while (c <= 10) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+
+//  inicialização   teste lógico  passo
+for (var c = 1;     c <= 10;      c++){
     console.log(`Passo ${c}`)
-    c++
 }
